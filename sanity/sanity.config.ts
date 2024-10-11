33b17924f9a5e115f3cfdfaec15eaf2a32723610
Reneazy <contactreneazy@gmail.com>
2024-10-12 03:10:43 +0530

    config

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'Reneazy Blog',
 
-  projectId: 'p7ai53ia',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '', // Get from env vars
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
 
   plugins: [structureTool(), visionTool()],
 
